64c65c68058dbdad216e27b3a4769bfaa75d9202
tanancuit06 <madiazf.ic@gmail.com>
2023-02-14 14:11:53 -0500

    Commit 14/02/2023-1: Centroid_2D.py and Centroid_2D_func.py are updated. Now Centroid_2D.py can run in a loop until the user decides to close it, there are several new options to use the script according to the user needs.

--- Test/Centroid_2D.py
+++ Test/Centroid_2D.py
@@ -6,100 +6,89 @@
 # centroid of any 2D object. It is thought to use the Gauss's Area Formula 
 # to calculate the simple polygon area.
 # 
-# Phase 1: Area calculation (05/02/2023)
-# Input: nodes coordinates
-# Method: Gauss's Area Formula
-# Output: area value
-# 
-# Phase 2: Area calculation (12/02/2023)
-# Input: nodes coordinates, area
-# Method: centroid of polygon formulas
-# Output: centroid coordinates
+# Final phase 
+# Input: option
+# Method: Centroid 2D functions (Centroid_2D_func.py)
+# Output: area and centroid of polygon
 #
 # Author: tanancuit06
 # Original date: 05/02/2023
-# Last modified date: 13/02/2023
+# Last modified date: 14/02/2023
 #---------------------------------------------------------------------------
 
 # Import lines
-import Structural_Classes as struct_clss
 import Centroid_2D_func
-import numpy as np
-import matplotlib.pyplot as plt
 
-# Node creation
+# Variables initialization
 node_list = []
-Centroid_2D_func.add_node(node_list,"0",1,1,0)
-Centroid_2D_func.add_node(node_list,"0",2,4,0)
-Centroid_2D_func.add_node(node_list,"0",5,5,0)
-Centroid_2D_func.add_node(node_list,"0",4,2,0)
-
-# Polygon coordinates arrays creation
 node_list_x = []
 node_list_y = []
 
-for i in range(len(node_list)):
-    node_list_x.append(node_list[i]["x"])
-    node_list_y.append(node_list[i]["y"])
-
-node_list_x.append(node_list_x[0])
-node_list_y.append(node_list_y[0])
-
-# Polygon drawing
-plt.plot(node_list_x,node_list_y)
-plt.title('Area of polygon')
-plt.xlabel("x-axis")
-plt.ylabel("y-axis")
-plt.show()
-
-node_list_x.pop()
-node_list_y.pop()
-
-print("Node list of x coordinates: ", node_list_x, "\n" + "Node list of y coordinates: ", node_list_y)
-
-# Gauss's Area Formula
-sum_x = 0
-for i in range(0,len(node_list_x)-1):
-    sum_x += node_list_x[i]*node_list_y[i+1]
-
-sum_y = 0
-for i in range(0,len(node_list_y)-1):
-    sum_y += node_list_x[i+1]*node_list_y[i]
-
-prod_1 = node_list_x[len(node_list_x)-1]*node_list_y[0]
-prod_2 = node_list_x[0]*node_list_y[len(node_list_y)-1]
-
-polygon_area = 0.5*np.abs(sum_x + prod_1 - sum_y - prod_2)
-print("Polygon area = ", polygon_area)
-
-# Centroid formulas
-
-node_list_x.append(node_list_x[0])
-node_list_y.append(node_list_y[0])
-
-Cx = 0
-for i in range(len(node_list_x)-1):
-    Cx += (node_list_x[i] + node_list_x[i+1]) * (node_list_x[i]*node_list_y[i+1] - node_list_x[i+1]*node_list_y[i])
-Cx = (1/(6*polygon_area))*abs(Cx)
-
-Cy = 0
-for i in range(len(node_list_y)-1):
-    Cy += (node_list_y[i] + node_list_y[i+1]) * (node_list_x[i]*node_list_y[i+1] - node_list_x[i+1]*node_list_y[i])
-Cy = (1/(6*polygon_area))*abs(Cy)
-
-print("Centroid x-coordinate: ", Cx, "\n" + "Centroid y-coordinate: ", Cy)
-
-'''
-# Program loop
 while(True):
-    print("Welcome. This script calculates the area of a polygon.",
-    "This can be used to calculate centroids. \n", 
+    print("")
+    print("Welcome. This script calculates the area of a polygon. This can be used to calculate centroids. \n", 
     "Your options are: \n",
     "A. Add a node to create a polygon. \n",
-    "C. Calculate the area of the polygon created by the nodes. \n",
+    "C. Calculate the area and centroid of the polygon created by the nodes. \n",
     "D. Draw and print the polygon. \n",
-    "R. Clean an existing polygon. \n",
+    "P. Print the current node list \n",
+    "R. Clear an existing polygon. \n",
     "Q. Quit \n")
     option = input("Option: ")
 
-'''
+    if option in ("A","a"):
+        node_id = input("Type the id of your node: ")
+        x_coord = float(input("Type the x-coordinate of your node: "))
+        y_coord = float(input("Type the y-coordinate of your node: "))
+        z_coord = float(input("Type the z-coordinate of your node: "))
+
+        # Node creation
+        Centroid_2D_func.add_node(node_list,node_id,x_coord,y_coord,z_coord)
+
+    elif option in ("C","c"):
+
+        # Polygon coordinates arrays creation
+
+        Centroid_2D_func.create_node_lists(node_list,node_list_x, node_list_y)
+
+        # Gauss's Area Formula
+        polygon_area = Centroid_2D_func.gauss_area_formula(node_list_x, node_list_y)
+
+        # Centroid formulas
+        node_list_x.append(node_list_x[0])
+        node_list_y.append(node_list_y[0])
+
+        Cx, Cy = Centroid_2D_func.centroid_coordinates(polygon_area, node_list_x, node_list_y)
+
+        node_list_x.pop()
+        node_list_y.pop()
+
+    elif option in ("D","d"):
+        # Polygon drawing
+        node_list_x.append(node_list_x[0])
+        node_list_y.append(node_list_y[0])
+
+        Centroid_2D_func.draw_polygon(node_list_x, node_list_y, Cx, Cy)
+
+        node_list_x.pop()
+        node_list_y.pop()
+
+    elif option in ("P","p"):
+        print(node_list)
+
+    elif option in ("R","r"):
+        node_list.clear()
+        node_list_x.clear()
+        node_list_y.clear()
+        print("node_list is now empty:", node_list)
+        print("node_list_x is now empty:", node_list_x)
+        print("node_list_y is now empty:", node_list_y)
+
+    elif option in ("Q","q"):
+        break
+
+    else:
+        print("Invalid option, select again.")
+        continue
+
+del node_list, node_list_x, node_list_y
--- Test/Centroid_2D_func.py
+++ Test/Centroid_2D_func.py
@@ -7,22 +7,75 @@
 # the geometric centroid of any 2D object. It is thought to use the Gauss's 
 # Area Formula to calculate the simple polygon area.
 # 
-# Phase 1: Area calculation
+# Phase 1: Area calculation (05/02/2023)
 # Input: nodes coordinates
-# Gauss's Area Formula
+# Method: Gauss's Area Formula
 # Output: area value
+# 
+# Phase 2: Centroid calculation (12/02/2023)
+# Input: nodes coordinates, area
+# Method: centroid of polygon formulas
+# Output: centroid coordinates
 #
 # Author: tanancuit06
-# Original date: 08/02/2023
-# Last modified date: 12/02/2023
+# Original date: 05/02/2023
+# Last modified date: 13/02/2023
 #---------------------------------------------------------------------------
 
 # Import lines
-import Structural_Classes as struct_clss
+import Structural_Classes
+import matplotlib.pyplot as plt
+import numpy as np
 
 # Functions
 def add_node(node_list,node_id,x,y,z):
-    new_node = struct_clss.Node(node_id,x,y,z)
+    new_node = Structural_Classes.Node(node_id,x,y,z)
     node_list.append(new_node.dict)
     del new_node
     return node_list
+
+def create_node_lists(node_list,node_list_x, node_list_y):
+    for i in range(len(node_list)):
+        node_list_x.append(node_list[i]["x"])
+        node_list_y.append(node_list[i]["y"])
+    print("Node list of x-coordinates: ", node_list_x, "\n" + "Node list of y-coordinates: ", node_list_y)
+    return node_list_x, node_list_y
+
+def draw_polygon(node_list_x, node_list_y,Cx,Cy):
+    plt.plot(node_list_x,node_list_y)
+    plt.plot(Cx,Cy,'r^')
+    plt.title('Area of polygon')
+    plt.xlabel("x-axis")
+    plt.ylabel("y-axis")
+    plt.show()
+
+def gauss_area_formula(node_list_x, node_list_y):
+    sum_x = 0
+    for i in range(0,len(node_list_x)-1):
+        sum_x += node_list_x[i]*node_list_y[i+1]
+
+    sum_y = 0
+    for i in range(0,len(node_list_y)-1):
+        sum_y += node_list_x[i+1]*node_list_y[i]
+
+    prod_1 = node_list_x[len(node_list_x)-1]*node_list_y[0]
+    prod_2 = node_list_x[0]*node_list_y[len(node_list_y)-1]
+
+    polygon_area = 0.5*np.abs(sum_x + prod_1 - sum_y - prod_2)
+    print("Polygon area = ", polygon_area)
+    return polygon_area
+
+def centroid_coordinates(polygon_area, node_list_x, node_list_y):
+    Cx = 0
+    for i in range(len(node_list_x)-1):
+        Cx += (node_list_x[i] + node_list_x[i+1]) * (node_list_x[i]*node_list_y[i+1] - node_list_x[i+1]*node_list_y[i])
+    Cx = (1/(6*polygon_area))*abs(Cx)
+    
+    Cy = 0
+    for i in range(len(node_list_y)-1):
+        Cy += (node_list_y[i] + node_list_y[i+1]) * (node_list_x[i]*node_list_y[i+1] - node_list_x[i+1]*node_list_y[i])
+    Cy = (1/(6*polygon_area))*abs(Cy)
+
+    print("Centroid x-coordinate: ", Cx, "\n" + "Centroid y-coordinate: ", Cy)
+
+    return Cx, Cy
